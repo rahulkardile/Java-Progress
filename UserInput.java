ca9639eb4cd49071c937fbc174b5f790c522c353
Rahul kardile <rahulkardile321@gmail.com>
2024-08-08 15:00:45 +0530

    learned about user input

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter your Age : ");
+        int age = Sc.nextInt();
+
+        System.out.println("Your age is : " + age);
+
+        Sc.close();
+
+    }
+}

cc42649562dd1aa35c0d3ab6b744c1ef4ae9d990
Rahul kardile <rahulkardile321@gmail.com>
2024-08-12 14:10:54 +0530

    array basic

--- arrays/BasicArray.java
+++ arrays/BasicArray.java
@@ -2,6 +2,17 @@ package arrays;
 
 public class BasicArray {
     public static void main(String[] args) {
-        
+        int age[];  // declerations
+        age = new int[5]; //allocation
+
+        age[0] = 25;
+        age[1] = 30;
+        age[2] = 35;
+        age[3] = 40;
+        age[4] = 45;
+    
+        System.out.println(age[1]);
+        System.out.println("This is the length of the array " + age.length);
+
     }
 }

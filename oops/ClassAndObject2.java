861f90fc8ad20ed01c4ab0755b45cc1db3eb295c
Rahul kardile <rahulkardile321@gmail.com>
2024-08-24 20:48:08 +0530

    learn constructore

--- oops/ClassAndObject2.java
+++ oops/ClassAndObject2.java
@@ -4,20 +4,47 @@ class Student {
     String name;
     Number age;
 
-    public void printInfo(){
+    public void printInfo() {
         System.out.println(this.name);
         System.out.println(this.age);
     }
+
+    // Constructor fun called
+    // This is non parametrized Constructor and this is the first of constructore
+    
+    Student() {
+        System.out.println("Constructore is called");
+    }
+
+    // this is second type of constructre
+    // parameterized constructore
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    // This is a copy constructore fun
+    // this constr. copy all the info from anther student abjects
+    Student(Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
+
 }
 
 public class ClassAndObject2 {
- public static void main(String[] args) {
-    Student s1 = new Student();
+    public static void main(String[] args) {
+        Student s1 = new Student();
 
-    s1.name = "Rahul";
-    s1.age = 21;
+        s1.name = "Rahul";
+        s1.age = 21;
+        s1.printInfo();
 
-    s1.printInfo();
+        Student s2 = new Student("Bhakti", 20);
+        s2.printInfo();
 
- }   
+        Student s3 = new Student(s2);
+        s3.printInfo();
+
+    }
 }

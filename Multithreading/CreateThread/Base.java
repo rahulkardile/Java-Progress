f0efda579262ee1005ead55688d71566956ab851
Rahul kardile <rahulkardile321@gmail.com>
2024-10-07 22:37:59 +0530

    multi threading with runable

--- Multithreading/CreateThread/Base.java
+++ Multithreading/CreateThread/Base.java
@@ -4,8 +4,14 @@ public class Base {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        // for extends thread
+        // World world = new World();
+        // world.start();
+
         World world = new World();
-        world.start();
+        Thread t1 = new Thread(world);
+
+        t1.start();
 
         // it will return the name of current thread name.
         for (;;) {
--- Multithreading/CreateThread/World.java
+++ Multithreading/CreateThread/World.java
@@ -1,6 +1,15 @@
 package Multithreading.CreateThread;
 
-public class World extends Thread {
+// public class World extends Thread {
+//     @Override 
+//     public void run(){
+//         for(; ;){
+//             System.out.println(Thread.currentThread().getName());
+//         }
+//     }
+// }
+
+public class World implements Runnable {
     @Override 
     public void run(){
         for(; ;){

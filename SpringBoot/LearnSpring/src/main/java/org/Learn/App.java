0e3d4bcc7e379fa19b96b10e715ea762223857f3
Rahul kardile <rahulkardile321@gmail.com>
2024-10-09 16:34:24 +0530

    Constructor and setter injection

--- SpringBoot/LearnSpring/src/main/java/org/Learn/App.java
+++ SpringBoot/LearnSpring/src/main/java/org/Learn/App.java
@@ -11,6 +11,7 @@ public class App
 
         Dev dev = (Dev) context.getBean("dev");
         dev.build();
+        System.out.println(dev.getAge());
 
     }
 }
--- SpringBoot/LearnSpring/src/main/java/org/Learn/Dev.java
+++ SpringBoot/LearnSpring/src/main/java/org/Learn/Dev.java
@@ -2,8 +2,36 @@ package org.Learn;
 
 public class Dev {
 
+    private int age;
+    private Laptop laptop;
+
     public Dev(){
         System.out.println("Dev Constructore . . . ");
+    }
+
+    public Dev(int age) {
+        this.age = age;
+    }
+
+    public Dev(Laptop laptop){
+        this.laptop = laptop;
+        System.out.println("Laptop Constructer from Dev . . . ");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
     }
 
     public void build(){

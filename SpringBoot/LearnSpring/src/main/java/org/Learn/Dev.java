e409e85c0c22ebcd5e9aaa408c2bda4b1c684d4f
Rahul kardile <rahulkardile321@gmail.com>
2024-10-09 07:58:43 +0530

    spring basics

---
+++ SpringBoot/LearnSpring/src/main/java/org/Learn/Dev.java
@@ -0,0 +1,13 @@
+package org.Learn;
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev Constructore . . . ");
+    }
+
+    public void build(){
+        System.out.println("Working on Awesome project . . . ");
+    }
+
+}
---
+++ SpringBoot/LearnSpring/src/main/java/org/Learn/Laptop.java
@@ -0,0 +1,12 @@
+package org.Learn;
+
+public class Laptop {
+
+   public Laptop(){
+        System.out.println("Laptop Constructore . . . ");
+    }
+
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}

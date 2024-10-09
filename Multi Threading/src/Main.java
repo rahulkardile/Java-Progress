642e0466723134e66c9c27b8f64e06f254ffc6b2
Rahul kardile <rahulkardile321@gmail.com>
2024-10-09 05:52:43 +0530

    spring boot inicial

--- Multi Threading/src/Main.java
+++ Multi Threading/src/Main.java
@@ -3,31 +3,22 @@ public class Main extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Running . . . ");  // Running State
-        try{
-            Thread.sleep(2000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+            try{
+                Thread.sleep(200);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
         }
+
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         Main t1 = new Main();
-
-        System.out.println(t1.getState());  // New State
-
         t1.start();
-
-        System.out.println(t1.getState());  // RUNNABLE State
-
-        Thread.sleep(100);
-
-        System.out.println(t1.getState()); //TIMED_WAITING
-
-        t1.join(); // waits untill t1 completes its execution
-
-        System.out.println(t1.getState());  // TERMINATED
-
+        t1.join(); // it will wait for t1 to finish.
+        System.out.println("Main find that t1 is finished");
     }
 }

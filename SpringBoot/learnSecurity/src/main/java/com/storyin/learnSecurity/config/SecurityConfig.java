f8b64b4e3cff2cea45295dd1156a51ed1f2c7143
Rahul kardile <rahulkardile321@gmail.com>
2024-10-24 15:10:59 +0530

    added user authentication

--- SpringBoot/learnSecurity/src/main/java/com/storyin/learnSecurity/config/SecurityConfig.java
+++ SpringBoot/learnSecurity/src/main/java/com/storyin/learnSecurity/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration  // this annotation tells spring that this is a configuration file.
@@ -24,12 +28,33 @@ public class SecurityConfig {
 //        };
 
         return httpSecurity
-                .csrf( customizer -> customizer.disable() )
-                .authorizeHttpRequests( req -> req.anyRequest().authenticated() )
-                .formLogin( Customizer.withDefaults() ) // it will provide form to login in browser
+                .csrf( customizer -> customizer.disable() )  // disabling csrf
+                .authorizeHttpRequests( req -> req.anyRequest().authenticated() )  // allows authentication
+//                .formLogin( Customizer.withDefaults() ) // it will provide form to login in browser
                 .httpBasic( Customizer.withDefaults() ) // it will allow authentication in postman also
                 .sessionManagement( session -> session.sessionCreationPolicy( SessionCreationPolicy.STATELESS ) ) // this will create new session each time.
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("suraj")
+                .password("suraj@123")
+                .roles("user")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("shubh")
+                .password("shubh@123")
+                .roles("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
     }
 
 }

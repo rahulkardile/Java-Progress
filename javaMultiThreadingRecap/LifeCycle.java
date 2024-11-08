9b20ba215291c7e991f9831b69ce7bef02387217
rahulkardile <rahulkardile321@gmail.com>
2024-11-08 12:17:09 +0530

    life cycle of a thread

---
+++ javaMultiThreadingRecap/LifeCycle.java
@@ -0,0 +1,33 @@
+
+// In This will explores all the the lifecycle of a thread
+
+public class LifeCycle extends Thread {
+
+    @Override
+    public void run() { //3. Running state 
+        System.out.println("RUNNING");
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        LifeCycle t1 = new LifeCycle();
+        System.out.println(t1.getState()); //1. New State
+
+        t1.start();
+        System.out.println(t1.getState());  //2. Runnable State
+  
+        Thread.sleep(100);
+
+        System.out.println(t1.getState()); // 4. time Waiting State
+
+        t1.join(); // it will wait to complete the execution of the t1 and then move forward.
+
+        System.out.println(t1.getState());  // 5. Terminated
+
+    }
+}
